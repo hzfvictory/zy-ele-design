cf2d96d4ecb7c0597a9f7ada4d83f1fd16673d77
hezhenfeng <feng960106@163.com>
2020-12-28 15:57:32 +0800

    :lipstick: 更新readme

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
-const PAGEPARAMS = {
+export const PAGEPARAMS = {
   pageSize: 1000, //每页显示多少条
   pageNum: 1 //当前页数
 };
-const STARTTIME = '00:00:00'
-const ENDTIME = '23:59:59'
+export const STARTTIME = '00:00:00'
+export const ENDTIME = '23:59:59'
